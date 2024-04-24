7d301c54ff20a1d9527134c2692fcb6d26e6879c
KudzanaiGomera <kudzie.gom@gmail.com>
2024-04-24 22:44:21 +0200

    commit on branch signup

--- local_connect/urls.py
+++ local_connect/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from local_connect import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('register', views.register, name='register'),
     path('community-hub', views.community_hub, name='community_hub'),
     path('business-profile', views.business_profile, name='business_profile'),
     path('profile', views.profile, name='profile'),
